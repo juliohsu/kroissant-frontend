92e36b11729a84e0086914c64a07f9ab29ff9131
juliohsu <tnt700900@gmail.com>
2024-11-12 16:30:35 -0300

    fixing lista compra selection

--- src/pages/ListaCompra/ListaCompra.tsx
+++ src/pages/ListaCompra/ListaCompra.tsx
@@ -163,7 +163,7 @@ function ListaCompra() {
                                                                                         item => (
                                                                                                 <>
                                                                                                     <ListItem 
-                                                                                                        key={item.prodId} 
+                                                                                                        key={item.itemId} 
                                                                                                         sx={
                                                                                                             { 
                                                                                                                 marginTop: 0.1, 
@@ -180,10 +180,10 @@ function ListaCompra() {
                                                                                                                 flexDirection: 'row',
                                                                                                                 width: '100vw'
                                                                                                             }}
-                                                                                                            onClick={() => handleItemToggle(item.prodId)}
+                                                                                                            onClick={() => handleItemToggle(item.itemId)}
                                                                                                         >
                                                                                                             <Checkbox 
-                                                                                                                checked={checked.includes(item.prodId)}
+                                                                                                                checked={checked.includes(item.itemId)}
                                                                                                                 color='secondary'
                                                                                                             />
                                                                                                             <Box>
@@ -207,7 +207,7 @@ function ListaCompra() {
                                                                                                                             {item.itemVencimento ?
                                                                                                                                 ` ; Vencimento: 
                                                                                                                                     ${new Date(item.itemVencimento).toLocaleDateString('pt-BR')} `
-                                                                                                                                : ''}
+                                                                                                                                : '; Sem Vencimento'}
                                                                                                                     ]
                                                                                                                 </Typography>
                                                                                                             </Box>
